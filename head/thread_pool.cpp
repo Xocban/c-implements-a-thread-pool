0112ba6ca6580d4eedd5b11d1b8fdf891ca026e6
xlyPro <drcnm666@163.com>
2022-09-09 21:35:22 +0800

    简易线程池

---
+++ head/thread_pool.cpp
@@ -0,0 +1,5 @@
+//
+// Created by River X on 2022/9/9.
+//
+
+#include "thread_pool.h"
---
+++ head/thread_pool.h
@@ -0,0 +1,14 @@
+//
+// Created by River X on 2022/9/9.
+//
+
+#ifndef THREADING_THREAD_POOL_H
+#define THREADING_THREAD_POOL_H
+
+
+class thread_pool {
+
+};
+
+
+#endif //THREADING_THREAD_POOL_H
